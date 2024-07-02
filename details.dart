32160d02a6b11e55538e8cc0d243069c888915f6
Feba <febanajim518@gmail.com>
2024-07-02 21:25:36 +0530

    committing

---
+++ details.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Feba Najim';
+  String email = 'abc@gmail.com';
+  String phone = '12345';
+
+  print('Name: $name');
+  print('Email : $email');
+  print('Phone : $phone');
+}
